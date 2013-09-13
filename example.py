73c9e40312a564a836acb2db7f8591c5a16108b9
chriswhsu <chriswhsu@berkeley.edu>
2013-09-13 16:02:53 -0700

    initial checking

---
+++ example.py
@@ -0,0 +1,86 @@
+__author__ = 'chriswhsu'
+#!/usr/bin/env python
+
+import logging
+from time import sleep
+from datetime import datetime
+
+
+log = logging.getLogger()
+log.setLevel('DEBUG')
+handler = logging.StreamHandler()
+handler.setFormatter(logging.Formatter("%(asctime)s [%(levelname)s] %(name)s: %(message)s"))
+log.addHandler(handler)
+
+from cassandra import ConsistencyLevel
+from cassandra.cluster import Cluster
+from cassandra.query import SimpleStatement
+
+KEYSPACE = "testkeyspace"
+
+def main():
+    cluster = Cluster(['127.0.0.1'])
+    session = cluster.connect()
+
+    rows = session.execute("SELECT keyspace_name FROM system.schema_keyspaces")
+    if KEYSPACE in [row[0] for row in rows]:
+        log.info("dropping existing keyspace...")
+        session.execute("DROP KEYSPACE " + KEYSPACE)
+
+    log.info("creating keyspace...")
+    session.execute("""
+        CREATE KEYSPACE %s
+        WITH replication = { 'class': 'SimpleStrategy', 'replication_factor': '2' }
+        """ % KEYSPACE)
+
+    log.info("setting keyspace...")
+    session.set_keyspace(KEYSPACE)
+
+    log.info("creating table...")
+    session.execute("""
+        CREATE TABLE mytable (
+            thekey text,
+            col1 text,
+            timepoint timestamp,
+            PRIMARY KEY (thekey, col1)
+        )
+        """)
+    log.info("created table")
+    query = SimpleStatement("""
+        INSERT INTO mytable (thekey, col1, timepoint)
+        VALUES (%(key)s, %(a)s, %(b)s)
+        """, consistency_level=ConsistencyLevel.ONE)
+
+    prepared = session.prepare("""
+        INSERT INTO mytable (thekey, col1, timepoint)
+        VALUES (?, ?, ?)
+        """)
+
+    log.info("created prepared statements")
+    for i in range(100):
+        # log.info("inserting row %d" % i)
+        # session.execute(query, dict(key="key%d" % i, a='cat', b=datetime.now()))
+        session.execute(prepared.bind(("key%d" % i, 'rat', datetime.now())))
+
+
+
+    log.info("completed inserts")
+    future = session.execute_async("SELECT * FROM mytable")
+
+    try:
+        rows = future.result()
+    except Exception:
+        log.exeception()
+
+    for row in rows:
+        print row
+
+    log.info("done")
+
+    session.execute("DROP KEYSPACE " + KEYSPACE)
+
+    session.shutdown()
+
+
+if __name__ == "__main__":
+    main()
