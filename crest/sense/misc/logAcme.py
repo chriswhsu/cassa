65cf0418c6e15c6e9bacd64d568d8f04fb2c9ba8
chriswhsu <chriswhsu@berkeley.edu>
2013-10-14 08:12:27 -0700

    do both reads and writes in separate threads to enable timeouts to be caught and processing to continue.

--- crest/sense/misc/logAcme.py
+++ crest/sense/misc/logAcme.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+TIMEOUT = 100
 
 __author__ = 'chriswhsu'
 import httplib
@@ -8,7 +9,7 @@ import logging
 import uuid
 import sys
 from multiprocessing.pool import ThreadPool
-from multiprocessing import TimeoutError
+from multiprocessing.pool import TimeoutError
 
 import pytz
 
@@ -16,59 +17,18 @@ from cassandra.cluster import Cluster
 
 
 def main(argv):
-
     def get_json():
 
-        log.info('------getting data with web request')
+        log.info('t-getting data with web request')
         conn.request("GET", "/data/+")
         r = conn.getresponse()
-        log.info('got response: ' + str(r.status) + ' ' + r.reason)
+        log.debug('t-got response: ' + str(r.status) + ' ' + r.reason)
         dat_str = r.read()
         results = json.loads(dat_str)
-        log.info('parsed into json.')
+        log.info('t-parsed into json.')
         return results
 
-    threadnum = int(argv[0])
-    insertcount = int(argv[1])
-
-    keyspace = "sense"
-
-    log = logging.getLogger()
-    log.setLevel('INFO')
-    handler = logging.StreamHandler()
-    handler.setFormatter(logging.Formatter("%(asctime)s [%(levelname)s] %(name)s: %(message)s"))
-    log.addHandler(handler)
-
-    conn = httplib.HTTPConnection("192.168.0.105", 8080)
-    cluster = Cluster(['128.32.189.230'], port=9042)
-    session = cluster.connect()
-
-    log.info("setting keyspace...")
-    session.set_keyspace(keyspace)
-
-    loop_count = 0
-
-    pool = ThreadPool(processes=1)
-
-    while True:
-        log.info("submitting async call")
-        async_result = pool.apply_async(get_json, ())
-
-        prepared = session.prepare("""
-                            Insert into data (device_id, day, tp, actenergy, actpower, aparpower)
-                            VALUES (?, ?, ?, ?, ?, ?)
-                            """)
-
-        log.debug("created prepared statements")
-        #create utc datetime so we can remove time portion.
-        utc = pytz.utc
-        # if this is the first time through need to get results before proceeding.
-        if loop_count == 0:
-            reading = async_result.get()
-            update_time = {m: 0 for m in reading['/costas_acmes']['Contents']}
-
-        loop_count += 1
-
+    def parse_write():
         for i in reading['/costas_acmes']['Contents']:
 
             i = str(i)
@@ -112,13 +72,12 @@ def main(argv):
                     utc_datetime = datetime.datetime.fromtimestamp(ts, utc)
                     utc_date = utc_datetime.replace(hour=0, minute=0, second=0, microsecond=0)
 
-                    prefix = 10000000 * threadnum
+                    fullprefix = 10000000 * prefix
 
-                    for p in range(prefix, prefix + insertcount + 1):
+                    for p in range(fullprefix, fullprefix + insertcount + 1):
                         dev_uuid = uuid.UUID(str(p) + '-0000-0000-0000-000000000' + i)
                         session.execute(prepared.bind((dev_uuid, utc_date, ts, energy, power, apparentpower)))
                         log.debug("executed prepared statements")
-
 
             else:
                 log.debug(i + ' skipping.')
@@ -126,16 +85,59 @@ def main(argv):
 
                 # time.sleep(0.1)
                 # end while loop (1 sec / loop)
-
         log.info("finished writing to DB")
 
-        if loop_count > 0:
-            # don't wait more than 300 ms, there are other readings to be had...
-            try:
-                reading = async_result.get(timeout=300)
-                log.info("got async results")
-            except TimeoutError:
-                log.info("timed out")
+    prefix = int(argv[0])
+    insertcount = int(argv[1])
+
+    keyspace = "sense"
+
+    log = logging.getLogger()
+    log.setLevel('INFO')
+    handler = logging.StreamHandler()
+    handler.setFormatter(logging.Formatter("%(asctime)s [%(levelname)s] %(name)s: %(message)s"))
+    log.addHandler(handler)
+
+    conn = httplib.HTTPConnection("192.168.0.105", 8080)
+    cluster = Cluster(['128.32.189.230'], port=9042)
+    session = cluster.connect()
+
+    log.info("setting keyspace...")
+    session.set_keyspace(keyspace)
+
+    loop_count = 0
+
+    pool = ThreadPool(processes=3)
+
+    prepared = session.prepare("""
+                            Insert into data (device_id, day, tp, actenergy, actpower, aparpower)
+                            VALUES (?, ?, ?, ?, ?, ?)
+                            """)
+
+    log.debug("created prepared statements")
+    #create utc datetime so we can remove time portion.
+    utc = pytz.utc
+
+    while True:
+        if loop_count == 0:
+            read1 = pool.apply_async(get_json, ())
+
+        try:
+            reading = read1.get(timeout=TIMEOUT)
+        except TimeoutError:
+            log.error("timeout reading")
+
+        if loop_count == 0:
+            update_time = {m: 0 for m in reading['/costas_acmes']['Contents']}
+
+        write1 = pool.apply_async(parse_write, ())
+        read1 = pool.apply_async(get_json, ())
+
+        try:
+            done_writing = write1.get(timeout=TIMEOUT)
+        except TimeoutError:
+            log.error("timeout writing")
+        loop_count += 1
 
     conn.close()
 
