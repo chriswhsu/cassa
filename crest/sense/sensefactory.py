45ebd25367b96763636156db742df0a81534e6f3
chriswhsu <chriswhsu@berkeley.edu>
2013-12-02 13:16:51 -0800

    pass test argument through buildworker

--- crest/sense/sensefactory.py
+++ crest/sense/sensefactory.py
@@ -4,8 +4,8 @@ from crest.sense import cassandraworker
 from crest.sense.device import Device
 
 
-def build_worker():
-    return cassandraworker.CassandraWorker(test=True)
+def build_worker(test=True):
+    return cassandraworker.CassandraWorker(test=test)
 
 
 def build_device(**kwargs):
