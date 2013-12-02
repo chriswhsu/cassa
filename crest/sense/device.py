b9b69c8ddbb99ddfd68a29f5952178270c6c6059
chriswhsu <chriswhsu@berkeley.edu>
2013-12-02 13:10:14 -0800

    try to get lowercase.

--- crest/sense/device.py
+++ crest/sense/device.py
@@ -1,1 +1,56 @@
 __author__ = 'chriswhsu'
+import uuid
+import geohash as gh
+
+
+class Device:
+
+    external_identifier = None
+    name = None
+    device_uuid = None
+    geohash = None
+    measures = None
+    tags = None
+    parent_device_uuid = None
+    latitude = None
+    longitude = None
+
+    def __init__(self, external_identifier, name, device_uuid=None, geohash=None, measures=None, tags=None,
+                 parent_device_uuid=None, latitude=None, longitude=None):
+        """ initialize new device object
+        """
+
+
+        # prevent inconsistant geospatial data from making it into repository
+        # rederive lat / long
+        if geohash and (latitude or longitude):
+            (latitude, longitude) = gh.decode(geohash)
+
+        # but populate both to facilitate queries that need lat/long data
+
+        if (latitude and longitude) and not geohash:
+            geohash = gh.encode(latitude, longitude)
+
+        if geohash and not (latitude or longitude):
+            (latitude, longitude) = gh.decode(geohash)
+
+        self.external_identifier = external_identifier
+        self.name = name
+        self.geohash = geohash
+        self.measures = measures
+        self.tags = tags
+        self.parent_device_id = parent_device_uuid
+        self.latitude = latitude
+        self.longitude = longitude
+
+        # get a uuid if one wasn't passed in.
+        if device_uuid is None:
+            self.device_uuid = uuid.uuid4()
+        elif isinstance(device_uuid, basestring):
+            self.device_uuid = uuid.UUID(device_uuid)
+        else:
+            self.device_uuid = device_uuid
+
+    def __str__(self):
+        return 'device_uuid: ' + str(
+            self.device_uuid) + ', name: ' + self.name + ', external_identifier: ' + self.external_identifier
